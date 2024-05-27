a91ba69158f0dd2fd83c2b33adcc8b5419c6db75
ariaandika <andikaakun17@gmail.com>
2024-05-27 21:24:26 +0700

    add: readme

---
+++ readme.md
@@ -0,0 +1,28 @@
+
+TODO:
+- [ ] get laptop brand
+- [ ] get ram information
+- [x] get storage information
+- [x] get partition, flags, mountpoint, unallocated, filesystem
+- [ ] get all installed os
+- [x] get battery percent
+- [ ] get internet connection
+- [ ] get bootloader
+- [x] get disk config:  
+    layout profiler
+    devices, ex: /dev/sda, /dev/sdb, etc
+    partition config, ex: fat: size, mountpoint, startsize & maxsize, flags
+- [ ] get hostname
+- [x] get kernel version
+- [ ] get keyboard layout
+- [ ] get nic (network interface)
+- [ ] get ntp
+- [ ] get packages
+- [ ] get timezone
+- [ ] get swap
+
+source:
+- `lspci`
+- dmidecode
+- lshw
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,6 @@
 //! Collection of api to get device info
 //!
 
-//! TODO:
-//! [ ] get laptop brand
-//! [ ] get ram information
-//! [ ] get storage information
-//! [ ] get partition, flags, mountpoint, unallocated, filesystem
-//! [ ] get all installed os
-//! [x] get battery percent
-//! [ ] get internet connection
-//! [ ] get bootloader
-//! [ ] get disk config:  
-//!   layout profiler
-//!   devices, ex: /dev/sda, /dev/sdb, etc
-//!   partition config, ex: fat: size, mountpoint, startsize & maxsize, flags
-//! [ ] get hostname
-//! [ ] get kernel version
-//! [ ] get keyboard layout
-//! [ ] get nic (network interface)
-//! [ ] get ntp
-//! [ ] get packages
-//! [ ] get timezone
-//! [ ] get swap
-//!
-
-//! source:
-//! - `lspci`
-//! - dmidecode
-//! - lshw
-
-
 pub mod disk;
 pub mod os;
 
